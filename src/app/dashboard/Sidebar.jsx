772d49a1037c5453373e40490fd4da6a22d87261
git config --global user.name tamzid620 <tamzid620@gmail.com>
2024-01-21 15:34:03 +0600

    sidebar sideLinks fixed

--- src/app/dashboard/Sidebar.jsx
+++ src/app/dashboard/Sidebar.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 
-const navLinks = [
+const sideLinks = [
   {
     path: "/dashboard",
     title: "Dashboard",
@@ -18,14 +18,14 @@ const navLinks = [
 
 const Sidebar = () => {
   return (
-    <aside className="mr-10">
-      <h1 className="text-green-500 text-3xl font-semibold">Dashboard</h1>
+<aside className="mr-10">
+      <h1 className="text-green-500 text-3xl font-semibold bg-slate-200">Dashboard</h1>
       <ul>
-        {navLinks.map(({ path, title }) => {
+        {sideLinks.map(({ path, title }) => (
           <li key={path}>
-            <Link>{title}</Link>
-          </li>;
-        })}
+            <Link href={path}>{title}</Link>
+          </li>
+        ))}
       </ul>
     </aside>
   );
---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="text-center text-red-500 ">
+      <h1 className="text-3xl font-bold">404</h1>
+      <h2 className="text-xl font-semibold">NotFound</h2>
+    </div>
+  );
+};
+
+export default NotFound;
